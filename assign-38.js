38e043d4999c50e2082ee8f5dc780f98491e72c6
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:12:34 +0500

    Q38

---
+++ assign-38.js
@@ -0,0 +1,12 @@
+// Write a function called describe_city() that accepts the name of a city and its country.
+// Give the parameter for the country a default value.
+function describe_city(city_name, country_name = "Pakistan") {
+    // The function should print a simple sentence, such as Karachi is in Pakistan.
+    console.log(`${city_name} is in ${country_name}`);
+}
+// Call your function for three different cities
+describe_city("Karachi");
+describe_city("Islamabad");
+// At least one of which is not in the default country.
+describe_city("Kabul", "Afghanistan");
+export {};
