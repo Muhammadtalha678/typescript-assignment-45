2331572a6e7e9db80821e23d5f79022c00e174eb
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:10:13 +0500

    Q23

---
+++ assign-23.js
@@ -0,0 +1,30 @@
+let count = 10;
+let car = "Corolla";
+let color = "blue";
+let friends = ['A', 'B', 'C'];
+let remainder = 12 % 5;
+// True Statement
+console.log("True Statement");
+console.log("Is count >= '10'?.This check value of count is greater than or equal to 10.I pridict it is True. ");
+console.log(count >= 10);
+console.log("Is car === 'Corolla'?.This check value of car and type(String) is equal to 'Corolla'.I pridict it is True. ");
+console.log(car === "Corolla");
+console.log("Is color != 'green'?.This check value of color  is not equal to 'gree'.I pridict it is True. ");
+console.log(color != "green");
+console.log("Is friends[0] == 'A'?.This check value of friends 0 index  is equal to 'A'.I pridict it is True. ");
+console.log(friends[0] == 'A');
+console.log("Is remainder > 0 ?.This check value of remainder greater than 0 .I pridict it is True. ");
+console.log(remainder > 0);
+// False Statement
+console.log("\nFalse Statement");
+console.log("Is count < '10'?.This check value of count is less than 10.I pridict it is False. ");
+console.log(count < 10);
+console.log("Is car === 'CD70'?.This check value of car and type(String) is equal to 'CD70'.I pridict it is False. ");
+console.log(car === "CD70");
+console.log("Is color == 'green'?.This check value of color  is  equal to 'gree'.I pridict it is False. ");
+console.log(color == "green");
+console.log("Is friends[1] == 'A'?.This check value of friends 1 index  is equal to 'A'.I pridict it is False. ");
+console.log(friends[1] == 'A');
+console.log("Is remainder <= 0 ?.This check value of remainder is less than 0 or equal to 0 .I pridict it is False. ");
+console.log(remainder <= 0);
+export {};
