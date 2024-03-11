3ef037419e8de1a6b0e35516308b9be957394f22
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:02:35 +0500

    Q11

---
+++ assign-11.ts
@@ -0,0 +1,23 @@
+let friends_name: string[] = ['Furqan', 'Farhan', 'Talha', 'Farhat']
+
+for (let index = 0; index < friends_name.length; index++) {
+    console.log(friends_name[index]);
+
+}
+export default friends_name
+
+// export class Employee {
+//     ids? : number
+//     constructor(public id: number, public name: string, public salary: number) {
+//         this.ids = id;
+//         this.name = name;
+//         this.salary = salary;
+//     }
+
+//     getSalary() {
+//         return this.salary;
+//     }
+// }
+
+// console.log(new Employee(1,"",2).salary);
+// console.log(new Employee(1,"",2).ids);
