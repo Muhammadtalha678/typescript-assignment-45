bbe20ac543e0b8abe65f15047d7a4cebe4de195d
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:10:53 +0500

    Q27

---
+++ assign-27.ts
@@ -0,0 +1,17 @@
+let alien_color3: string = "yellow"
+
+if (alien_color3 == 'green') {
+    console.log("player earned 5 points.");
+
+}
+else if (alien_color3 == 'yellow') {
+    console.log("player earned 10 points.");
+    
+}
+else if (alien_color3 == 'red') {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log('Player not earned any point');
+    
+}
