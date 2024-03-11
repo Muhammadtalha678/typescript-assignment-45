a6ba01464e19780f712c919c43cc274b8fbd0c16
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:11:24 +0500

    Q30

---
+++ assign-30.js
@@ -0,0 +1,10 @@
+let userNames = ['Ali', 'Taha', 'Mubeen', 'Hassan', 'Kainat', 'admin', 'Talha', 'Furqan'];
+for (let index = 0; index < userNames.length; index++) {
+    if (userNames[index] == 'admin') {
+        console.log('\n"Hello admin, would you like to see a status report"\n');
+    }
+    else {
+        console.log(`Hello ${userNames[index]}, thank you for logging in again.`);
+    }
+}
+export {};
