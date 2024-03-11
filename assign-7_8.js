6849b6d723d8ae8c95f3d6ac7953482c60573535
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:01:23 +0500

    Q7,Q8

---
+++ assign-7_8.js
@@ -0,0 +1,9 @@
+let addition = 5 + 3;
+console.log(`Addition: 5 + 3 = ${addition}`);
+let subtraction = 10 - 2;
+console.log(`Subtraction: 10 - 2 = ${addition}`);
+let multiplication = 2 * 4;
+console.log(`Multiplication: 2 * 4 = ${multiplication}`);
+let division = 16 / 2;
+console.log(`Division: 16 / 2 = ${division}`);
+export {};
---
+++ assign-7_8.ts
@@ -0,0 +1,12 @@
+
+let addition: number = 5 + 3
+console.log(`Addition: 5 + 3 = ${addition}`);
+
+let subtraction: number = 10 - 2
+console.log(`Subtraction: 10 - 2 = ${addition}`);
+
+let multiplication: number = 2 * 4
+console.log(`Multiplication: 2 * 4 = ${multiplication}`);
+
+let division: number = 16 / 2
+console.log(`Division: 16 / 2 = ${division}`);
