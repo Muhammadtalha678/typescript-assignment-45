0650955ce49ab72ad3ee6c0317f042f4c350d8f5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:10:37 +0500

    Q25

---
+++ assign-25.js
@@ -0,0 +1,7 @@
+let alien_color = "green";
+if (alien_color == 'green') {
+    console.log('Player earned 5 points.');
+}
+if (alien_color != 'red') {
+}
+export {};
---
+++ assign-25.ts
@@ -0,0 +1,8 @@
+let alien_color: string = "green"
+if (alien_color == 'green') {
+    console.log('Player earned 5 points.');
+
+}
+if (alien_color != 'red') {
+
+}
