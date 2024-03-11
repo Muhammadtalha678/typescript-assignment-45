94e451fc193d665064f842664dde171d63b0c53b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:12:41 +0500

    Q39

---
+++ assign-39.ts
@@ -0,0 +1,12 @@
+//  Write a function called city_country() that takes in the name of a city and its country.
+
+function city_country(city_name: string, country_name: string) {
+    // The function should return a string formatted like this:
+    // "Lahore, Pakistan"
+    return `"${city_name}, ${country_name}"`
+}
+
+// Call your function with at least three city - country pairs, and print the value that’s returned.
+console.log(city_country("Karachi","Pakistan"));
+console.log(city_country("Gilgit Baltistan","Pakistan"));
+console.log(city_country("Sibi","Pakistan"));
