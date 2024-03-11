7c107e58a10e1483d3bf151eec620f59b806231f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:10:46 +0500

    Q26

---
+++ assign-26.ts
@@ -0,0 +1,19 @@
+let alien_color2: string = "green"
+if (alien_color2 == 'green') {
+    console.log('Player earned 5 points points for shooting the alien.');
+
+}
+alien_color2 = "red"
+if (alien_color2 != 'green') {
+
+    console.log('Player earned 10 points.\n');
+}
+alien_color2 = 'blue'
+if (alien_color2 == 'green') {
+    console.log('Player earned 5 points.');
+
+}
+else {
+    console.log('Player earned 10 points.');
+
+}
