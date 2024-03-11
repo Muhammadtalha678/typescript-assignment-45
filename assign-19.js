f74dd3eb975f55223669b63176586d6347a8ecf4
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:08:42 +0500

    Q19

---
+++ assign-19.js
@@ -0,0 +1,11 @@
+import guest_list from "./assign-14.js";
+import ChangingGuestList from "./assign-15.js";
+// Q 14
+console.log(`Iam inviting only ${guest_list.length} people in Q14 assignment`);
+console.log("\n");
+// Q 15
+console.log(`Iam inviting only ${ChangingGuestList.length} people in Q15 assignment`);
+console.log("\n");
+// Q 16
+console.log(`Iam inviting only ${ChangingGuestList.length} people in Q15 assignment`);
+console.log("\n");
