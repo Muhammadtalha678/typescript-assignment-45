99dc9aac9bb1ca58903d4ad65305a44da9ad2749
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:11:33 +0500

    Q31

---
+++ assign-31.ts
@@ -0,0 +1,14 @@
+let user_names: string[] = ['Ali', 'Taha', 'Mubeen', 'Hassan', 'Kainat', 'admin', 'Talha', 'Furqan']
+// make sure the list of users is not empty.
+if (user_names.length > 0) {
+    console.log("User list is not empty");
+    // Remove all the usernames from the array
+    user_names = [];
+
+    // Print a message confirming the removal
+    console.log("All usernames have been removed.");
+}
+else {
+    console.log("If the list is empty, print the message We need to find some users!");
+
+}
