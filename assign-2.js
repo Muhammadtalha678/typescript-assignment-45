c5afad010207a4527470c52fa26e365297562a30
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-11 23:58:28 +0500

    Q2

---
+++ assign-2.js
@@ -0,0 +1,5 @@
+let personName = "Muhammad Talha";
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
+// OR
+console.log("Hello " + personName + ", would you like to learn some Python today?");
+export {};
---
+++ assign-2.ts
@@ -0,0 +1,4 @@
+let personName = "Muhammad Talha";
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
+// OR
+console.log("Hello " + personName + ", would you like to learn some Python today?");
