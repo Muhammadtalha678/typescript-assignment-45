c2a3107e119fb557448ffd8d8127550bdedd7df5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:02:46 +0500

    Q12

---
+++ assign-12.js
@@ -0,0 +1,5 @@
+import friends_name from "./assign-11.js";
+console.log("\n");
+for (let index = 0; index < friends_name.length; index++) {
+    console.log(`Asslamalikum, ${friends_name[index]}, you are in my friends list`);
+}
