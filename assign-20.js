b2254697c24acf2a3da048b3b05a15de47d8e70c
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:08:52 +0500

    Q20

---
+++ assign-20.js
@@ -0,0 +1,4 @@
+let fruits = ['Apple', 'Banana', 'Orange', 'Strawberry', 'Grapes'];
+console.log("List of fruits");
+export {};
+// console.log();
---
+++ assign-20.ts
@@ -0,0 +1,3 @@
+let fruits: string[] = ['Apple', 'Banana', 'Orange', 'Strawberry', 'Grapes'];
+console.log("List of fruits");
+// console.log();
