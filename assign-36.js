1db1920bee7f52a89118676ee4436b85a6d7b7ed
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:12:15 +0500

    Q36

---
+++ assign-36.js
@@ -0,0 +1,9 @@
+// T - Shirt: Write a function called make_shirt() that accepts a size and the text of a message that
+// should be printed on the shirt.
+function make_shirt(size, message) {
+    // The function should print a sentence summarizing the size of the shirt and the message printed on it.
+    console.log(`The size of the shirt is "${size}" and the message print on the shirt is "${message}"`);
+}
+//  Call the function.
+make_shirt(34, "Muhammad Talha");
+export {};
