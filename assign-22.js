6da586f949160a8bd8a7e46119df6ab8d3c3f8c4
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:10:05 +0500

    Q22

---
+++ assign-22.js
@@ -0,0 +1,4 @@
+let error = ['error1', 'error2', 'error3', 'error4', 'error5'];
+console.log(error[10]);
+console.log(error[4]);
+export {};
---
+++ assign-22.ts
@@ -0,0 +1,6 @@
+let error: string[] = ['error1', 'error2', 'error3', 'error4', 'error5'];
+
+console.log(error[10]);
+
+
+console.log(error[4]);
