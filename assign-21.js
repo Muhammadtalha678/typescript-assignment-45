a4b23de2a8fb233f5dff8488c94e8d58933e5ab7
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-12 00:09:58 +0500

    Q21

---
+++ assign-21.js
@@ -0,0 +1,21 @@
+let person1 = {
+    name: "Talha",
+    age: 24,
+    gender: 'male'
+};
+// or
+let person2;
+person2 = {
+    name: "furqan",
+    age: 28,
+    gender: "male"
+};
+console.log("Person1 name = " + person1.name);
+console.log("Person1 age = " + person1.age);
+console.log("Person1 gender = " + person1.gender + "\n");
+console.log("Person2 name = " + person2.name);
+console.log("Person2 age = " + person2.age);
+console.log("Person2 gender = " + person2.gender);
+export {};
+// console.log("Person2 gender = "+person2.gender);
+// console.log(typeof(dd))
---
+++ assign-21.ts
@@ -0,0 +1,24 @@
+let person1: { name: string, age: number, gender:string }= {
+    name: "Talha",
+    age: 24,
+    gender: 'male'
+}
+// or
+
+let person2: { name: string, age: number, gender: string }
+
+person2 = {
+    name : "furqan",
+    age: 28,
+ gender:"male"
+}
+
+    console.log("Person1 name = "+person1.name);
+    console.log("Person1 age = "+person1.age);
+    console.log("Person1 gender = "+person1.gender + "\n");
+    
+    console.log("Person2 name = "+person2.name);
+    console.log("Person2 age = "+person2.age);
+    console.log("Person2 gender = "+person2.gender);
+    // console.log("Person2 gender = "+person2.gender);
+    // console.log(typeof(dd))
