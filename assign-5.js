ed530948cd8f304ac2ffbd2ae898f74c0856cf35
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-11 23:59:23 +0500

    Q5

---
+++ assign-5.js
@@ -0,0 +1,4 @@
+let famous_person = "Tipu Sultan";
+let message = "To live like a lion for a day is far better than to live for a hundred years like a jackal.";
+console.log(`${famous_person} once said, "${message}."`);
+export {};
---
+++ assign-5.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Tipu Sultan"
+
+let message = "To live like a lion for a day is far better than to live for a hundred years like a jackal."
+
+console.log(`${famous_person} once said, "${message}."`);
