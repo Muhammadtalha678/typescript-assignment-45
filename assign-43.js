0a7093db5d466a4168e0a0f6812579f002bd090f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-03-13 00:14:11 +0500

    Q43

---
+++ assign-43.js
@@ -0,0 +1,25 @@
+/*Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array
+ of magicians’ names.Because the original array will be unchanged, return the new array and store it in a separate
+array.Call show_magicians() with each array to show that you have one array of the original names and one array
+with the Great added to each magician’s name.*/
+function make_great(magiciansName) {
+    let modifiedArray = [];
+    for (let index = 0; index < magiciansName.length; index++) {
+        modifiedArray.push(`${magiciansName[index]} the great`);
+    }
+    return modifiedArray;
+}
+function show_magicians(magicianName) {
+    for (let index = 0; index < magicianName.length; index++) {
+        console.log(magicianName[index]);
+    }
+}
+const originalArray = ['Talha', 'Farhan', 'Furqan'];
+// call the old array to make great function to modified with the great word
+const newArray = make_great(originalArray);
+// Call show_magicians() with each array to show that you have one array of the original names and one array
+// with the Great added to each magician’s name.
+show_magicians(originalArray);
+console.log('\n');
+show_magicians(newArray);
+export {};
